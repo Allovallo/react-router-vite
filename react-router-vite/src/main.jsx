26db2477326b6d860ea8b53564e6b1eb8c9e86c5
Allovallo <grossco@meta.ua>
2024-09-02 16:59:59 +0300

    files updated

--- react-router-vite/src/main.jsx
+++ react-router-vite/src/main.jsx
@@ -7,7 +7,7 @@ import {
 import Root, {loader as rootLoader, action as rootAction} from "./routes/root";
 import "./index.css";
 import ErrorPage from "./error-page";
-import Contact from "./routes/contact";
+import Contact, {loader as contactLoader} from "./routes/contact";
 
 const router = createBrowserRouter([
   {
@@ -19,7 +19,8 @@ const router = createBrowserRouter([
     children: [
       {
         path: 'contacts/:contactId',
-        element: <Contact/>,
+        element: <Contact />,
+        loader: contactLoader,
       }
     ]
   },
