d99e46b07ee41807d1bee88adfc769a57b01fc94
Allovallo <grossco@meta.ua>
2024-09-02 11:02:33 +0300

    files updated

--- react-router-vite/src/main.jsx
+++ react-router-vite/src/main.jsx
@@ -1,10 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from "./routes/root";
+import "./index.css";
+import ErrorPage from "./error-page";
+import Contact from "./routes/contact";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage/>,
+  },
+  {
+    path: 'contacts/:contactId',
+    element: <Contact/>,
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
