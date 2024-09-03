39ceaee59ada9d4d3ebd14f5ccea0ceb5f0ae42b
Allovallo <grossco@meta.ua>
2024-09-03 16:19:41 +0300

    files updated

--- react-router-vite/src/routes/index.jsx
+++ react-router-vite/src/routes/index.jsx
@@ -1,7 +1,7 @@
 export default function Index() {
     return (
         <p id='zero-state'>
-            There is a demo fir React Router
+            There is a demo for React Router
             <br />
             Check out{' '}
             <a href="https://reactrouter.com">
