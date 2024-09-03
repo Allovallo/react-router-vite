b8825fcf1eb431dae1d8c015025069408d29ba8c
Allovallo <grossco@meta.ua>
2024-09-03 11:18:36 +0300

    Index Routes updated

---
+++ react-router-vite/src/routes/index.jsx
@@ -0,0 +1,13 @@
+export default function Index() {
+    return (
+        <p id='zero-state'>
+            There is a demo fir React Router
+            <br />
+            Check out{' '}
+            <a href="https://reactrouter.com">
+                the docs at reactrouter.com
+            </a>
+            .
+        </p>
+    );
+}
